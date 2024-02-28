8343faf62a4280f35001295d94272a14a12d8455
SayedNehalAhmad <116965622+SayedNehalAhmad@users.noreply.github.com>
2024-02-28 22:23:57 +0530

    Add files via upload

---
+++ diseasePrediction.py
@@ -0,0 +1,71 @@
+
+
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+diabetesModel = pickle.load(open('D:/Codes/machine_learning/diseasePrediction/diabetes.sav', 'rb'))
+
+
+with st.sidebar:
+    selected = option_menu('Disease Prediction System',
+                           ['Diabetes Prediction'],
+
+                           )
+
+
+# Diabetes Prediction Page
+if selected == 'Diabetes Prediction':
+
+    # page title
+    st.title('Diabetes Prediction using ML')
+    st.radio('Select gender',['Male','Female'])
+
+    # getting the input data from the user
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        Pregnancies = st.text_input('Number of Pregnancies')
+
+    with col2:
+        Glucose = st.text_input('Glucose Level')
+
+    with col3:
+        BloodPressure = st.text_input('Blood Pressure value')
+
+    with col1:
+        SkinThickness = st.text_input('Skin Thickness value')
+
+    with col2:
+        Insulin = st.text_input('Insulin Level')
+
+    with col3:
+        BMI = st.text_input('BMI value')
+
+    with col1:
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+
+    with col2:
+        Age = st.text_input('Age of the Person')
+
+
+    # code for Prediction
+    diab_diagnosis = ''
+
+    # creating a button for Prediction
+
+    if st.button('Diabetes Test Result'):
+
+        user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
+                      BMI, DiabetesPedigreeFunction, Age]
+
+        user_input = [float(x) for x in user_input]
+
+        diab_prediction = diabetesModel.predict([user_input])
+
+        if diab_prediction[0] == 1:
+            diab_diagnosis = 'The person is diabetic'
+        else:
+            diab_diagnosis = 'The person is not diabetic'
+
+    st.success(diab_diagnosis)
